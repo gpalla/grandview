5cc52d98e4e5e5e3e2d1546188d131a79eb907a9
Geni Palla <palla.inbox@gmail.com>
2017-03-13 16:30:49 -0400

    update
    1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,31 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { BoardComponent } from './board/board.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { HomeComponent } from './home/home.component';
+
+const appRoutes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'board', component: BoardComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BoardComponent,
+    PageNotFoundComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/board/board.component.ts
@@ -0,0 +1,95 @@
+import { Component, OnInit } from '@angular/core';
+
+export class Member {
+  id: string;
+  name: string;
+  short_desc: string;
+  long_desc: string;
+  start_date: Date;
+  end_date: Date;
+  status: Boolean;
+  img_url: string;
+  committee: string[];
+}
+
+const GVMembers: Member[] = [
+  {
+    id: 'gxp',
+    name: 'Geni Palla',
+    short_desc: 'Program Developer @ MapAnything.',
+    long_desc: 'I am a computer programmer that focuses on web based applications. ' +
+    'I build production ready web applications on a variety of platforms using the latest standards such as: ' +
+    'Angular 2 JS, Node JS, and Bootstrap. I also use legacy languages such as ASP.net and JSF. ' +
+    'Feel free to reach out to me for any of your IT needs.',
+    start_date: new Date(2017, 3, 12),
+    end_date: null,
+    status: true,
+    img_url: '../assets/images/members/geni.jpg',
+    committee: ['IT-Committee', 'Sustainability-Committee']
+  },
+  {
+    id: 'exm',
+    name: 'Ed Mosely',
+    short_desc: 'short test ed mosely',
+    long_desc: 'long test ed mosely',
+    start_date: new Date(2010, 5, 14),
+    end_date: new Date(2017, 3, 12),
+    status: true,
+    img_url: '../assets/images/members/unknown.jpg',
+    committee: ['IT-Committee', 'Sustainability-Committee']
+  },
+  {
+    id: 'kxf',
+    name: 'Ken Farrington',
+    short_desc: 'short test Ken Farrington',
+    long_desc: 'long test Ken Farrington',
+    start_date: new Date(2010, 5, 14),
+    end_date: new Date(2017, 3, 12),
+    status: true,
+    img_url: '../assets/images/members/unknown.jpg',
+    committee: ['IT-Committee', 'Sustainability-Committee']
+  },
+  {
+    id: 'sxd',
+    name: 'Steven Darst',
+    short_desc: 'short test Steven Darst',
+    long_desc: 'long test Steven Darst',
+    start_date: new Date(2010, 5, 14),
+    end_date: new Date(2017, 3, 12),
+    status: true,
+    img_url: '../assets/images/members/unknown.jpg',
+    committee: ['IT-Committee', 'Sustainability-Committee']
+  },
+  {
+    id: 'jxd',
+    name: 'Janet King',
+    short_desc: 'short test Janet King',
+    long_desc: 'long test Janet King',
+    start_date: new Date(2010, 5, 14),
+    end_date: new Date(2017, 3, 12),
+    status: true,
+    img_url: '../assets/images/members/unknown.jpg',
+    committee: ['IT-Committee', 'Sustainability-Committee']
+  }
+];
+
+@Component({
+  selector: 'app-board',
+  templateUrl: './board.component.html',
+  styleUrls: ['./board.component.css']
+})
+
+export class BoardComponent implements OnInit {
+  title = 'Grandview Board Members';
+  members = GVMembers;
+
+  sendContact(id) {
+    alert(id);
+  }
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
